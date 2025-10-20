99855c92f450c297c3d32da956f5abbbd515087d
chreid1973 <chreid@gmail.com>
2025-10-19 20:49:15 -0600

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import App from './app'; // NOTE: lowercase if your file is app.tsx
+import './index.css';   // if you have a global css; otherwise remove
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
